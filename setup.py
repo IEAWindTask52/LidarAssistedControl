c1def1e038a0e160f82994846e3eca013946d126
Aravind V <43172847+aravindvenkatachalapathy@users.noreply.github.com>
2024-04-15 18:55:08 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='LidarAssistedControl',
+    url='https://github.com/IEAWindTask52/LidarAssistedControl',
+    author='David Schlipf, Aravind v, Simon Weich',
+    python_requires=">=3.10",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scipy",
+        "joblib"
+    ]
+)
