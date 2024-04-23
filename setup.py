60ea7aa6b8df2f398d6fc554f8fc36418477b716
DavidSchlipf <david.schlipf@hs-flensburg.de>
2024-04-23 09:26:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name='LidarAssistedControl',
     url='https://github.com/IEAWindTask52/LidarAssistedControl',
-    author='Feng Guo,David Schlipf, Aravind v, Simon Weich',
     python_requires=">=3.10",
     packages=find_packages(),
     install_requires=[
