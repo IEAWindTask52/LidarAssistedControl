c09eb04715b01324ea0f4e5a5da06d8a771f4d84
Aravind V <43172847+aravindvenkatachalapathy@users.noreply.github.com>
2024-04-23 09:11:05 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='LidarAssistedControl',
     url='https://github.com/IEAWindTask52/LidarAssistedControl',
-    author='David Schlipf, Aravind v, Simon Weich',
+    author='Feng Guo,David Schlipf, Aravind v, Simon Weich',
     python_requires=">=3.10",
     packages=find_packages(),
     install_requires=[
