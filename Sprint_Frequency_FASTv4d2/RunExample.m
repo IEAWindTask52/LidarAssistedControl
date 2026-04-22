d332c51c70ed63d0d65d72bc21a71e764bb9bc69
Julius Preuschoff <76872934+Plutoniumdrache@users.noreply.github.com>
2026-04-22 09:22:01 +0200

    FrequencyFolder

---
+++ Sprint_Frequency_FASTv4d2/RunExample.m
@@ -0,0 +1,70 @@
+% IEA 15 MW monopile + perfect wind preview.
+% Origin and changes in files: see ChangeLogOpenFAST.txt.
+% Purpose:
+% Here, we use a perfect wind preview to demonstrate that the collective
+% pitch feedforward controller (designed with SLOW) is able to reduce
+% significantly the rotor speed variation when OpenFAST is disturbed by an
+% Extreme Operating Gust. Here, only the rotor motion and tower motion 
+% (GenDOF and TwFADOF1) are enabled.  
+% Result:       
+% Cost ("30 s sprint"):  0.952481
+
+%% Setup
+clearvars;close all;clc;
+addpath(genpath('..\WetiMatlabFunctions'))
+
+% define FAST input file
+SimulationName  = 'IEA-15-240-RWT-Monopile';
+
+%% Run FB and FF simulation
+dos(['openfast_x64_v4d2.exe ',SimulationName,'_FB.fst']); 
+dos(['openfast_x64_v4d2.exe ',SimulationName,'_FBFF.fst']);
+
+%% Comparison
+% read in data
+FB                  = ReadFASTbinaryIntoStruct([SimulationName,'_FB.outb']);
+FBFF                = ReadFASTbinaryIntoStruct([SimulationName,'_FBFF.outb']);
+FBFF_R              = ReadROSCOtextIntoStruct([SimulationName,'_FBFF.RO.dbg']);
+
+% Plot 
+figure('Name','Simulation results')
+
+subplot(4,1,1);
+hold on; grid on; box on
+plot(FB.Time,       FB.Wind1VelX);
+plot(FBFF_R.Time,   FBFF_R.REWS);
+ylabel('[m/s]');
+legend('Wind1VelX','REWS preview','Interpreter','none','Location','best')
+
+subplot(4,1,2);
+hold on; grid on; box on
+plot(FB.Time,       FB.BldPitch1);
+plot(FBFF.Time,     FBFF.BldPitch1);
+ylabel({'BldPitch1'; '[deg]'});
+legend('feedback only','feedback-feedforward','Location','best')
+
+subplot(4,1,3);
+hold on; grid on; box on
+plot(FB.Time,       FB.RotSpeed);
+plot(FBFF.Time,     FBFF.RotSpeed);
+ylabel({'RotSpeed';'[rpm]'});
+
+subplot(4,1,4);
+hold on; grid on; box on
+plot(FB.Time,       FB.TwrBsMyt/1e3);
+plot(FBFF.Time,     FBFF.TwrBsMyt/1e3);
+ylabel({'TwrBsMyt';'[MNm]'});
+
+xlabel('time [s]')
+linkaxes(findobj(gcf, 'Type', 'Axes'),'x');
+xlim([0 30])
+
+% display results
+RotSpeed_0  = 7.56;     % [rpm]
+TwrBsMyt_0  = 158.3e3;  % [kNm]
+t_Start     = 0;        % [s]
+
+Cost = (max(abs(FBFF.RotSpeed(FBFF.Time>=t_Start)-RotSpeed_0))) / RotSpeed_0 ...
+     + (max(abs(FBFF.TwrBsMyt(FBFF.Time>=t_Start)-TwrBsMyt_0))) / TwrBsMyt_0;
+
+fprintf('Cost ("30 s sprint"):  %f \n',Cost);
