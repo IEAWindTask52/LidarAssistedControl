35b001da8fa5aa9dd312c2a28b24916a2cff5318
Julius Preuschoff <76872934+Plutoniumdrache@users.noreply.github.com>
2026-04-23 14:54:31 +0200

    working example

--- Sprint_Frequency_FASTv4d2/Frequency/GenerateFrequencySignal.m
+++ Sprint_Frequency_FASTv4d2/Frequency/GenerateFrequencySignal.m
@@ -1,80 +1,59 @@
-clear;close all;
-
-%% Generate frequency signal with noise
+% Script to generate different frequency signals for the use of the PFC
+% DLL.
+clear;close all;clc;
+%% Preprocessing
 
 % params
 Tmax = 40; % [s]
 dt = 0.01; % [s]
 f0 = 50; % [Hz] grid frequency
 
-snr = 105; % [] signal to noise ratio
-
-% params for delta torque calculation
-H = 1; % [s] system equivalent inertia
-S = 15e6; % [VA] rated power
-T = -2 * S * H / f0; % 
-Omega_rated = rpm2radPs(7.56); % [rpm]
+% noise parameters
+SNR         = 70; % [-] signal to noise ratio
+AvgWindow   = 100; % [-] window size for noise averaging
+% triangle parameters
+shape = 1/2; % 1/2 = triangle
+amplitude = 0.2; 
 
 % generate base signal
 t = 0:dt:Tmax-dt;
-f_constant = ones(1,length(t))*f0;
 
-% add noise
-% f = awgn(f_constant,snr); 
-rng(1); % initialize random number generator to get the same results every time
-P_signal = mean(f_constant.^2);
-P_noise = P_signal / 10^(snr/10);
-noise = sqrt(P_noise) * randn(size(t));
-NoiseSmooth = movmean(noise,51);
-f_noisy = f_constant + NoiseSmooth;
-f = f_noisy;
+% select signal type
+SignalType = "noisy"; % possible: triangle, noisy
+
+switch SignalType
+    case "noisy"
+        f_constant = ones(1,length(t))*f0;
+        % add noise
+        rng(1); % initialize random number generator to get the same results every time
+        P_signal = mean(f_constant.^2);
+        P_noise = P_signal / 10^(SNR/10);
+        noise = sqrt(P_noise) * randn(size(t));
+        NoiseSmooth = movmean(noise,AvgWindow);
+        f_noisy = f_constant + NoiseSmooth;
+        f = f_noisy;
+    case "triangle"
+        f = sawtooth(t,shape)*amplitude + f0;
+    otherwise
+        error("No case with this name")
+end
+
 % calculate RoCoF
 RoCoF = diff(f)./dt;
-% determine torque update
-delta_P = T * RoCoF;
-delta_MgFF = delta_P * (1/Omega_rated);
 
-% plot overview
+% Plot signal
 figure("Name","FrequencySignalGeneration")
-subplot(411)
+subplot(211)
 hold on; grid on; box on
 plot(t,f);
 ylabel("f [Hz]")
 
-subplot(412)
+subplot(212)
 hold on; grid on; box on
 plot(t(1:end-1),RoCoF);
 ylabel("RoCoF [Hz/s]")
 
-subplot(413)
-hold on; grid on; box on
-plot(t(1:end-1),delta_MgFF/1e3);
-ylabel("delta MgFF [MNm]")
-xlabel("time [s]")
-
-subplot(414)
-hold on; grid on; box on
-plot(t(1:end-1),delta_P/1e3);
-ylabel("delta P [MW]")
-xlabel("time [s]")
-
-% print some statistics
-fprintf("max f = %f\n", max(f));
-fprintf("min f = %f\n", min(f));
-fprintf("max RoCoF = %f\n", max(RoCoF));
-fprintf("min RoCoF = %f\n", min(RoCoF));
-fprintf("max delta P = %f\n", max(delta_P/1e3));
-fprintf("min delta P = %f\n", min(delta_P/1e3));
-fprintf("max delta MgFF = %f\n", max(delta_MgFF/1e3));
-fprintf("min delta MgFF = %f\n", min(delta_MgFF/1e3));
-%% write signal to csv file
+%% Generate csv file
 % frequency
 m = [t(1:end-1);f(1:end-1)]';
-writematrix(m,"FrequencySignalTest.csv");
-% delta torque
-n = [t(1:end-1);delta_MgFF]';
-% writematrix(n,"DeltaTorqueFFSignalTest.csv");
-
-fid = fopen("DeltaTorqueFFSignalTest.csv",'w+');
-fprintf(fid,['%f,%f\r\n'],[t(1:end-1)',delta_MgFF']');
-fclose(fid);
+writematrix(m,"FrequencySignal.csv");
--- Sprint_Frequency_FASTv4d2/RunExample.m
+++ Sprint_Frequency_FASTv4d2/RunExample.m
@@ -1,13 +1,9 @@
-% IEA 15 MW monopile + perfect wind preview.
-% Origin and changes in files: see ChangeLogOpenFAST.txt.
-% Purpose:
-% Here, we use a perfect wind preview to demonstrate that the collective
-% pitch feedforward controller (designed with SLOW) is able to reduce
-% significantly the rotor speed variation when OpenFAST is disturbed by an
-% Extreme Operating Gust. Here, only the rotor motion and tower motion 
-% (GenDOF and TwFADOF1) are enabled.  
-% Result:       
-% Cost ("30 s sprint"):  0.952481
+% IEA 15 MW monopile + perfect wind preview + PFC. Origin and changes in
+% files: see ChangeLogOpenFAST.txt. Purpose: Here, we use perfect wind
+% preview + a PFC module to demonstrate how an FF torque updates can be 
+% utilized for PFC. Here, only the rotor motion and tower motion 
+% (GenDOF and TwFADOF1) are enabled. 
+% Result: Cost ("30 s sprint"):  0.952481
 
 %% Setup
 clearvars;close all;clc;
@@ -68,3 +64,43 @@ Cost = (max(abs(FBFF.RotSpeed(FBFF.Time>=t_Start)-RotSpeed_0))) / RotSpeed_0 ...
      + (max(abs(FBFF.TwrBsMyt(FBFF.Time>=t_Start)-TwrBsMyt_0))) / TwrBsMyt_0;
 
 fprintf('Cost ("30 s sprint"):  %f \n',Cost);
+
+%% Additional plots
+% import frequency signal
+f = readmatrix("Frequency\FrequencySignal.csv");
+
+% Plot 
+figure('Name','Simulation results with FF torque update')
+
+subplot(4,1,1);
+hold on; grid on; box on
+plot(FB.Time,       FB.Wind1VelX);
+plot(FBFF_R.Time,   FBFF_R.REWS);
+ylabel('[m/s]');
+legend('Wind1VelX','REWS preview','Interpreter','none','Location','best')
+title("inputs")
+
+subplot(4,1,2);
+hold on; grid on; box on
+plot(f(1:3001,1),f(1:3001,2))
+ylabel("f [Hz]")
+legend("grid frequency")
+
+subplot(4,1,3);
+hold on; grid on; box on
+% plot(FB.Time,       FB.TwrBsMyt/1e3);
+plot(FBFF_R.Time,     FBFF_R.FF_TorqueUpdate/1e3);
+ylabel({'[kNm]'});
+legend("FF_{TorqueUpdate}")
+title("outputs")
+
+subplot(4,1,4);
+hold on; grid on; box on
+plot(FB.Time,       FB.GenTq/1e3);
+plot(FBFF.Time,     FBFF.GenTq/1e3);
+ylabel({'GenTq'; '[MNm]'});
+legend('feedback only','feedback-feedforward','Location','best')
+
+xlabel('time [s]')
+linkaxes(findobj(gcf, 'Type', 'Axes'),'x');
+xlim([0 30])
